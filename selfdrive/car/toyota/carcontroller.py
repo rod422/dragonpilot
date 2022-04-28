d8b52dcc7c21888cfc37e0c35d991a28225473c9
CT9212 <mlb.tim001@gmail.com>
2022-04-28 18:36:42 +0800

    Fix toyota-steer-faults

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -13,13 +13,15 @@ from common.dp_common import common_controller_ctrl
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
+STEER_FAULT_MAX_RATE = 100
+STEER_FAULT_MAX_FRAMES = 18
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     # dp
     self.last_blinker_on = False
     self.blinker_end_frame = 0.
-
+    self.rate_limit_counter = 0
     self.last_steer = 0
     self.alert_active = False
     self.last_standstill = False
@@ -61,12 +63,21 @@ class CarController():
     apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, CarControllerParams)
     self.steer_rate_limited = new_steer != apply_steer
 
-    # Cut steering while we're in a known fault state (2s)
-    if not active or CS.steer_state in (9, 25) or abs(CS.out.steeringRateDeg) > 100 or (abs(CS.out.steeringAngleDeg) > 150 and CS.CP.carFingerprint in [CAR.RAV4H, CAR.PRIUS]):
+    # EPS_STATUS->LKA_STATE either goes to 21 or 25 on rising edge of a steering fault and
+    # the value seems to describe how many frames the steering rate was above 100 deg/s, so
+    # cut torque with some margin for the lower state
+    if active and abs(CS.out.steeringRateDeg) >= STEER_FAULT_MAX_RATE:
+      self.rate_limit_counter += 1
+    else:
+      # TODO: unclear if it resets its internal state at another value
+      self.rate_limit_counter = 0
+
+    apply_steer_req = 1
+    if not active:
       apply_steer = 0
       apply_steer_req = 0
-    else:
-      apply_steer_req = 1
+    elif self.rate_limit_counter > STEER_FAULT_MAX_FRAMES:
+      apply_steer_req = 0
 
     # TODO: probably can delete this. CS.pcm_acc_status uses a different signal
     # than CS.cruiseState.enabled. confirm they're not meaningfully different
