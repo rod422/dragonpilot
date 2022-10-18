9f56b5281ad4c51e823383c9738a9cde8efcdbdd
CT921 <tim.chen242@gmail.com>
2022-10-18 21:27:12 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -38,7 +38,7 @@ def get_max_accel(v_ego, CP):
       a_cruise_max_bp = [0., 10., 25., 40.]
     else:
       a_cruise_max_vals = [2.0, 1.7, 1.4, 1.2, 0.7, 0.6]  # Sets the limits of the planner accel, PID may exceed
-      a_cruise_max_bp = [0., 3, 6., 8., 10., 25., 40.]
+      a_cruise_max_bp = [0., 3., 6., 8., 10., 25., 40.]
     return interp(v_ego, a_cruise_max_bp, a_cruise_max_vals)
   else:
     return interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)
